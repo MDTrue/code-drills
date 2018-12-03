872b408146a294e4bc9065d7eeae7eef932dccfe
MDTrue <MDTrueII@hotmail.com>
2018-12-03 00:23:54 -0800

    firebase code drills pushed to master

--- 07-firebase-codedrills/06-core-firebase-chat/challenge-prompt.js
+++ 07-firebase-codedrills/06-core-firebase-chat/challenge-prompt.js
@@ -1,5 +1,8 @@
 // Firebase Chat
 
+
+  // Initialize Firebase
+
 // creating a variable that references our database, so we don't have to type
 // it out every time
 var database = firebase.database();
@@ -8,11 +11,22 @@ var database = firebase.database();
 $(function() {
 
   // -------------------- Your Code Here --------------------
+  // console.log("hello")
 
+  database.ref().on("child_added", function(childSnapshot){
+    var chatItem = $("<p>")
+  chatItem.text(childSnapshot.val())
+  $("#chat-display").append(chatItem)
+})
+  $(document).on("click", "#chat-submit",function(event){
+    event.preventDefault();
+    var chatMessage = $("#chat-input").val().trim()
 
+    $("#chat-input").val("") 
+
+    database.ref().push(chatMessage)
+  })
   
-  
- 
 
   // --------------------- End Code Area --------------------
 
